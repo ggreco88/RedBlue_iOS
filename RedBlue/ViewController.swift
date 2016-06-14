7a4ec1624b587226513b869a59f67fbf289711ce
Guillermo <elguillemex@Greco.local>
2016-06-14 09:46:52 -0700

    Initial Commit

---
+++ RedBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedBlue
+//
+//  Created by Guillermo on 6/14/16.
+//  Copyright (c) 2016 Guillermo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
